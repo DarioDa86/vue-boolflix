46e21cbaf26c469bb04e93513bf445f577d9590b
DarioDa86 <dario.dagnelli@gmail.com>
2021-10-08 15:13:44 +0200

    Aggiunto stile

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -1,27 +1,30 @@
 <template>
-        <ul class="film-card">
-            <li>
+        <div class="film-card">
+            <div class="poster-box">
                 <img v-if="info.poster_path != null" :src="`https://image.tmdb.org/t/p/w342${info.poster_path}`">
                 <img v-else :src="`https://thumbs.dreamstime.com/b/punto-interrogativo-20881124.jpg`" alt="">
-            </li>
-            <li>{{info.title || info.name}}</li>
-            <li>{{info.original_title || info.original_name}}</li>
-            <li >{{info.original_language}} 
-                <img :src="flag(info.original_language)">
-            </li>
-            <li>
-                <span class="star"
-                    v-for="(stars, index ) in starsNumber() " :key="index">
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                </span>
+            </div>
+            <ul class="film-info">
+                <li><span class="txt-bold">Titolo: </span>{{info.title || info.name}}</li>
+                <li><span class="txt-bold">Titolo originale: </span>{{info.original_title || info.original_name}}</li>
+                <li ><span class="txt-bold">Lingua originale: </span>
+                    <img class="flag-style" :src="flag(info.original_language)">
+                </li>
+                <li >
+                    <span class="star"
+                        v-for="(stars, index ) in starsNumber() " :key="index">
+                        <font-awesome-icon :icon="['fas', 'star']" />
+                    </span>
 
-                <span class="star-border" 
-                    v-for="(stars, index ) in ( 5 - starsNumber()) " :key="index">
-                    <font-awesome-icon :icon="['far', 'star']" />
-                </span>
-            </li>
-        </ul>
+                    <span class="star-border" 
+                        v-for="(stars, index ) in ( 5 - starsNumber()) " :key="index">
+                        <font-awesome-icon :icon="['far', 'star']" />
+                    </span>
+                </li>
+            </ul>
+        </div>
 </template>
+
 
 <script>
 export default {
@@ -29,7 +32,7 @@ export default {
     props: ['info'],
     data() {
         return {
-            noFlag: [ 'iu', 'jv', 'kg', 'ki','te', 'tl', 'kj', 'gu', 'ii', 'ik', 'ml', 'mr',  'nn', 'nr', 'pi', 'ps', 'sa', 'nb', 'nd', 'ng', 'sw',  'tw']
+            noFlag: ['iu', 'jv', 'kg', 'ki','te', 'tl', 'kj', 'gu', 'ii', 'ik', 'ml', 'mr',  'nn', 'nr', 'pi', 'ps', 'sa', 'nb', 'nd', 'ng', 'sw',  'tw']
         }
     },
 
@@ -47,8 +50,56 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+.film-card {
+    width: calc(20% - 20px);
+    margin: 10px;
+    position: relative;
 
+    .poster-box {
+        width: 100%;
+        overflow: hidden;
+        border-radius: 5px;
+        position: relative;
+
+        img {
+            border-radius: 5px;
+            width: 100%;
+            opacity: 1;
+            transition: opacity .3s;
+
+        }
+        img:hover {
+            opacity: 0;        
+        }
+    }
+
+    .film-info {
+        position: absolute;
+        transition: 0.2s;
+        opacity: 1;
+        top: 10px;
+
+        li {
+            list-style: none;
+            margin: 10px 5px;
+    
+            .txt-bold{
+                font-weight: 600;
+            }
+    
+            .flag-style {
+                width: 40px;
+                border-radius: 5px;
+            }
+    
+            .star-border, .star {
+                color: yellow;
+            }
+        }
+    }
+
+}
 
 </style>
--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class=" container">
+    <div class="container">
         <h2 class="film-title">I Film </h2>
-        <font-awesome-icon :icon="['far', 'star']" />
-        <font-awesome-icon :icon="['fas', 'star']" />
+        <div class="results-container">
+            <!-- <font-awesome-icon :icon="['far', 'star']" />
+            <font-awesome-icon :icon="['fas', 'star']" /> -->
+        <div  class="results-container">
+            <Film  v-for="(film, index) in films" :key="index" :info="film" />
+        </div>
 
-        <ul>
-            <li v-for="(film, index) in films" :key="index">
-                <Film :info="film" />
-            </li>
-        </ul>
+        </div>
         <h2>Le serie TV</h2>
-        <ul>
-            <li v-for="(film, index) in tvSeries" :key="index">
-                <Film :info="film" />
-            </li>
-        </ul>
+        <div class="results-container">
+            <Film v-for="(film, index) in tvSeries" :key="index" :info="film" />
+        </div>
     </div>
 </template>
 
@@ -31,6 +29,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+h2 {
+font-size: 40px;
+margin: 30px 0;
+}
+
+.results-container {
+    display: flex;
+    flex-wrap: wrap;
+    
+    li {
+        list-style: none;
+    }
+}
+
 
 </style>
